fe55c89097ea3c13cb8a9f5a83aeeffa3ba5a02e
nyakazat <st10445682@rcconnect.edu.za>
2026-04-13 12:53:09 +0200

    Update Utilities.cs

--- CyberSecurityChatbot/Utilities.cs
+++ CyberSecurityChatbot/Utilities.cs
@@ -5,7 +5,7 @@ namespace CyberSecurityChatbot
 {
     public static class Utilities
     {
-        public static void TypeText(string text, int delay = 20)
+        public static void TypeText(string text, int delay = 2)
         {
             foreach (char character in text)
             {

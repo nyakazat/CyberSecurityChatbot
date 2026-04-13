7a46a0696c7145945afe04ddaa7b45e677107f05
nyakazat <st10445682@rcconnect.edu.za>
2026-04-13 12:53:24 +0200

    Contains helper functions such as the typing effect.

--- CyberSecurityChatbot/Utilities.cs
+++ CyberSecurityChatbot/Utilities.cs
@@ -5,7 +5,7 @@ namespace CyberSecurityChatbot
 {
     public static class Utilities
     {
-        public static void TypeText(string text, int delay = 2)
+        public static void TypeText(string text, int delay = 20)
         {
             foreach (char character in text)
             {
